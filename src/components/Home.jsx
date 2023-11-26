8612675f1151f14e14db0019bedc9367c15c84d7
Priya Gupta <priyagupta052k@gmail.com>
2023-11-26 23:44:57 +0530

    Navbar added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,17 @@
-import React from "react";
-
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
 function Home (){
     return (
-        <div>Home Page</div>
+        <div className="container">
+           <AppBar position="static">
+           <div className="logo">Logo</div>
+              <ul className="menu">
+                  <li>About us</li>
+                  <li>Solutions</li>
+                  <li>Pricing</li>
+              </ul>
+           </AppBar> 
+        </div>
     );
 }
 
