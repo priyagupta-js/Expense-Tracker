ec39cd3fbf3611e9a0b9c25b4ea388aee1faf916
Priya Gupta <priyagupta052k@gmail.com>
2023-11-29 12:48:51 +0530

    Navbar CSS modified

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import  Home from "./pages/Home";
 import About from "./pages/About";
 import Solutions from "./pages/Solutions";
 import Pricing from "./pages/Pricing";
-
+import Contact from "./pages/Contact";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <Introduction />
+      {/* <Introduction /> */}
       <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/about" element={<About />} />
       <Route path="/solutions" element={<Solutions />} />
       <Route path="/pricing" element={<Pricing />} />
+      <Route path="/contact" element={<Contact />} />
 
     </Routes>
     </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,18 @@
 import React from "react";
-import { Container, Navbar, Nav } from "react-bootstrap/";
+import { Container, Navbar, Nav ,Button} from "react-bootstrap/";
 import { NavLink} from "react-router-dom";
 function NavBar() {
   return (
-    <Navbar bg="light" data-bs-theme="light">
+    <Navbar sticky="top" data-bs-theme="light">
       <Container>
-        <Navbar.Brand as={NavLink} to="/">Navbar</Navbar.Brand>
+        <Navbar.Brand className="logo" as={NavLink} to="/">Expense</Navbar.Brand>
         <Nav className="me-auto">
           <Nav.Link as={NavLink} to="/about">About us</Nav.Link>
           <Nav.Link as={NavLink} to="/solutions">Solutions</Nav.Link>
           <Nav.Link as={NavLink} to="/pricing">Pricing</Nav.Link>
+          <Nav.Link as={NavLink} to="/contact">Contact us</Nav.Link>
         </Nav>
+        <Button variant="primary">Login/Sign up</Button>
       </Container>
     </Navbar>
   );
