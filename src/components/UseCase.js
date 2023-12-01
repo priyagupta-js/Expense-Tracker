471674fdfea65ba0d783db35fce0b57457a32ad4
Priya Gupta <priyagupta052k@gmail.com>
2023-12-01 20:38:23 +0530

    Use Case section added

---
+++ src/components/UseCase.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Card, Container, Row, Col } from "react-bootstrap";
+
+function UseCase() {
+  return (
+    <Container className="useCase-Container">
+      <h3>See how Expense can help you manage your everyday expenses.</h3>
+      <Row>
+        <Col>
+          <Card>
+            <Card.Text>icon</Card.Text>
+            <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+          <Card.Text>icon</Card.Text>
+            <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+        <Col>
+          <Card>
+          <Card.Text>icon</Card.Text>
+            <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+export default UseCase;
